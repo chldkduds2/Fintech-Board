0ae59d1156b1b73f7fa366f0f037fd5e9e082a8a
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2024-08-21 08:49:27 +0900

    FIX : 파일 세팅 오류 수정

---
+++ src/components/Common/FindaBoardLayout/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import type { PropsWithChildren } from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+const FindaBoardLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default FindaBoardLayout;
